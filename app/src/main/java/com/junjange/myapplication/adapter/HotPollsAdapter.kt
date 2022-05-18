09770e7810dfe2760fb3519c5933cf2905961462
JunJangE <wnswkd486@gmail.com>
2022-05-18 21:12:01 +0900

    22.05.18Update

--- app/src/main/java/com/junjange/myapplication/adapter/HotPollsAdapter.kt
+++ app/src/main/java/com/junjange/myapplication/adapter/HotPollsAdapter.kt
@@ -3,9 +3,11 @@ package com.junjange.myapplication.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.junjange.myapplication.R
 import com.junjange.myapplication.data.ModelBoard
 import com.junjange.myapplication.databinding.ItemRecyclerHotPollsBinding
 import com.junjange.myapplication.databinding.ItemRecyclerPollsBinding
@@ -24,6 +26,9 @@ class HotPollsAdapter(val context: Context) : RecyclerView.Adapter<HotPollsAdapt
 
     // 전달받은 위치의 아이템 연결
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.rankNumber(position)
+
+
 
     }
 
@@ -36,9 +41,18 @@ class HotPollsAdapter(val context: Context) : RecyclerView.Adapter<HotPollsAdapt
                 // 데이터 전달
 //                intent.putExtra("key", value)
                 context.startActivity(intent) //액티비티 열기
+            }
 
+        }
 
-
+        @SuppressLint("SetTextI18n")
+        fun rankNumber(position: Int){
+            binding.rank.text = "${position + 1}"
+            when (position) {
+                0 -> binding.rank.setBackgroundResource(R.drawable.layout_rank1)
+                1 ->  binding.rank.setBackgroundResource(R.drawable.layout_rank2)
+                2 -> binding.rank.setBackgroundResource(R.drawable.layout_rank3)
+                else -> binding.rank.setBackgroundResource(R.drawable.layout_rank4)
             }
         }
 
--- app/src/main/java/com/junjange/myapplication/adapter/PhotoVoteAdapter.kt
+++ app/src/main/java/com/junjange/myapplication/adapter/PhotoVoteAdapter.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.junjange.myapplication.data.ModelBoard
+import com.junjange.myapplication.data.ModelBoardComponent
 import com.junjange.myapplication.databinding.ImteRecyclerPhotoVoteBinding
 import com.junjange.myapplication.databinding.ItemRecyclerQuickVoteBinding
 
-class PhotoVoteAdapter : RecyclerView.Adapter<PhotoVoteAdapter.ViewHolder>(){
+class PhotoVoteAdapter(val onClickListener: ItemClickListener) : RecyclerView.Adapter<PhotoVoteAdapter.ViewHolder>(){
 
     private var items: ModelBoard = ModelBoard(ArrayList())
 
+    interface ItemClickListener {
+        fun onPhotoVoteClickListener(item: ModelBoardComponent, position: Int)
+    }
 
     // 뷰 홀더 만들어서 반환
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -25,11 +29,22 @@ class PhotoVoteAdapter : RecyclerView.Adapter<PhotoVoteAdapter.ViewHolder>(){
     // 전달받은 위치의 아이템 연결
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-
+        holder.bind(items.board[position], position)
     }
 
     // 뷰 홀더 설정
     inner class ViewHolder(private val binding: ImteRecyclerPhotoVoteBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: ModelBoardComponent, position: Int) {
+
+
+            binding.root.setOnClickListener {
+
+                onClickListener.onPhotoVoteClickListener(item, position)
+
+
+            }
+        }
 
     }
 
